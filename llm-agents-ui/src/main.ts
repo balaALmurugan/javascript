268214ae06d91a973d318aa0bc77e4a02396c52a
BalaMurugan <bala27895@gmail.com>
2025-08-13 00:47:37 +0530

    changes are done

--- llm-agents-ui/src/main.ts
+++ llm-agents-ui/src/main.ts
@@ -1,6 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
+import { ChatDashboardComponent } from './app/chat/chat';
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(ChatDashboardComponent)
   .catch((err) => console.error(err));
